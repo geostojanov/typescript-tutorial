1d44ee866e8da649cdd02e5a832e38b6a1825d65
George <gestojanov@gmail.com>
2023-06-18 14:31:33 +0200

    commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import { Payment } from './models/Payment.js'
+import { Invoice } from './models/Invoice.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
+import { ListTemplate } from './models/ListTemplate.js';
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+  
+  let values: [string, string, number];
+  values = [toFrom.value, details.value, amount.valueAsNumber];
+  
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values)
+  } else {
+    doc = new Payment(...values)
+  }
+  
+  list.render(doc, type.value, 'end');
+});
